0f85a68bde415680663ed21796e01b7bb0aa5ec4
Guillermo Hernandez <ghernandez@andrewharper.com>
2016-08-31 15:24:34 -0500

    updated blurb on main view

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -9,9 +9,5 @@
  */
 angular.module('jerkfolioApp')
   .controller('ContactCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -9,9 +9,5 @@
  */
 angular.module('jerkfolioApp')
   .controller('ProjectsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
